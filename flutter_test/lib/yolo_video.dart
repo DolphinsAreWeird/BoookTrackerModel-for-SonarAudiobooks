00eaf4fff7c5d98496a8726feec491637d60cd98
l-krairit <124411749+l-krairit@users.noreply.github.com>
2025-02-03 11:57:40 +0700

    fixed dependencies

--- flutter_test/lib/yolo_video.dart
+++ flutter_test/lib/yolo_video.dart
@@ -339,7 +339,6 @@ class DetectionPainter extends CustomPainter {
 import 'package:flutter_vision/flutter_vision.dart';
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_tts/flutter_tts.dart';
 
 late List<CameraDescription> cameras;
 
